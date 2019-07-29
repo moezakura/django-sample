2c703f1794dc4e57c22655303f91ea0e6538e1bb
Masaki Furukawa <mox@g.mox.si>
2019-07-30 05:58:07 +0900

    バリデーションを追加

--- myproject/view.py
+++ myproject/view.py
@@ -18,18 +18,31 @@ def post(request: WSGIRequest):
         return HttpResponse("not allow method", status=405)
 
     error_message = ''
-    try:
-        text = request.POST['text']
-        name = request.POST['name']
-        post = Post.objects.create(text=text, name=name, post_date=datetime.now())
-        post.save()
-    except:
-        error_message = '投稿処理に失敗しました'
+    text = request.POST['text']
+    name = request.POST['name']
+
+    if len(name) == 0:
+        error_message = '名前を入力してください'
+    elif len(text) == 0:
+        error_message = '内容を入力してください'
+    elif len(name) > 40:
+        error_message = '名前は40文字以内で入力してください'
+    elif len(text) > 65535:
+        error_message = '内容は65535文字以内で入力してください'
+
+    if error_message == '':
+        try:
+            post = Post.objects.create(text=text, name=name, post_date=datetime.now())
+            post.save()
+        except:
+            error_message = '投稿処理に失敗しました'
 
     template = loader.get_template('myproject/index.html')
     return HttpResponse(template.render({
         'error_message': error_message,
         'posts': getPosts(),
+        'name': name,
+        'text': text,
     }, request))
 
 
